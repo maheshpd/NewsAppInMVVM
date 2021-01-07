a951bbf7b155f49c7b22232dfeac897928441da5
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-01-07 11:49:57 +0530

    Setting up Navigation - MVVM News App - Part 2

--- app/src/main/java/com/createsapp/newsappinmvvm/NewsActivity.kt
+++ app/src/main/java/com/createsapp/newsappinmvvm/NewsActivity.kt
@@ -2,10 +2,22 @@ package com.createsapp.newsappinmvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class NewsActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var newsNavHostFragment: Fragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
     }
 }
